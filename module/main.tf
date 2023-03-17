96c136cbafec5d057ba0f343c9fcd3d44d981b22
Pitambarbittu <99995315+Pitambarbittu@users.noreply.github.com>
2023-03-17 20:58:28 +0530

    Finally done

--- module/main.tf
+++ module/main.tf
@@ -1,9 +1,12 @@
-
-module "sm_ec2" {
+module "ec2" {
   source = "../ec2"
 
   count = 2
   ami_id = var.ami_id
   inst_type = var.inst_type
   tag_name = var.tag_name
+}
+
+module "s3" {
+  source = "../s3"
 }
